a9fa8c5b5726261d66494db0759aee9f535dc30a
Pashani Jaulani <pashanijaulani@gmail.com>
2022-11-14 16:23:26 +0200

    Password Resset
    Password Resetting Feature

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('express-async-handler');
+const crypto = require('crypto')
 
 const maxAge = 3 * 24 * 60 * 60;
 
@@ -54,6 +55,48 @@ const authUser = asyncHandler( async (req, res)=>{
             id: user._id
         })
     }
-})
+});
 
-module.exports= {registerUser, authUser};
+const recover = async (req, res)=>{
+    const email = req.body.email;
+    User.findOne({email})
+        .then(user=>{
+            if(!user) return res.status(401).json({message: 'this email is not registered'});
+            user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+            user.resetPasswordExpires = Date.now() + 3600000;
+            user.save();
+            console.log(user.resetPasswordToken)
+            res.send("http://" + req.headers.host + "/reset/" + user.resetPasswordToken);
+        })
+        .catch(err => res.status(500).json({message: err.message}));
+}
+const reset = (req, res)=>{
+    const token = req.params.token;
+    User.findOne({
+        resetPasswordToken: token,
+        resetPasswordExpires: {$gt: Date.now()}
+    })
+    .then((user)=>{
+        if(!user) return res.status(401).json({message: 'Password reset token is invalid or has expired.'});
+        res.send(user)
+    })
+    .catch(error => res.status(500).json({message: error.message})); 
+};
+
+const resetPassword = (req, res) =>{
+    User.findOne({
+        resetPasswordToken: req.params.token,
+        resetPasswordExpires: {$gt: Date.now()}
+    })
+    .then((user)=>{
+        user.password = req.body.password;
+        user.resetPasswordToken= undefined;
+        user.resetPasswordExpires= undefined;
+        user.save((error)=>{
+            if(error){ return res.status(500).json({message: error.message});}
+            else{ return res.status(201).json({message: 'success password has been reset'})}
+        })
+    })
+};
+
+module.exports= {registerUser, authUser, recover, reset, resetPassword};
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 const Schema = mongoose.Schema;
 
@@ -25,6 +27,14 @@ const UserSchema = new Schema({
         type : Boolean,
         required: true,
         default: false,
+    },
+    resetPasswordToken:{
+        type: String,
+        required: false
+    },
+    resetPasswordExpires:{
+        type: Date,
+        required:false,
     }
 },
 {
@@ -41,6 +51,27 @@ UserSchema.pre('save', async function(next){
 
 UserSchema.methods.matchPassword = async function (enteredPassword){
     return await bcrypt.compare(enteredPassword, this.password);
+}
+
+UserSchema.methods.generateJWT = ()=>{
+    const today = new Date();
+    const expirationDate = new Date(today);
+    expirationDate.setDate(today.getDate() + 60);
+
+    let payload = {
+        id: this._id,
+        email: this.email,
+        firstname: this.email,
+        lastname: this.lastname,
+    }
+    return jwt.sign(payload, process.env.SECRET_KEY, {
+        expiresIn: parseInt(expirationDate.getTime() / 1000, 10)
+    });
+};
+
+UserSchema.methods.generatePasswordReset = async ()=>{
+    this.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+    this.resetPasswordExpires = Date.now() + 3600000;
 }
 
 const User = mongoose.model('User', UserSchema);
